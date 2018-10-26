ca498101f5e9ffc6bd152d8e24bb21e263100b03
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-10-26 11:59:16 -0700

    Complete Phase 1 of Poly Tree Node

---
+++ skeleton/lib/00_tree_node.rb
@@ -0,0 +1,34 @@
+require 'byebug'
+
+class PolyTreeNode
+  
+  attr_reader :parent
+  
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+  
+  def parent=(parent_node)
+    if parent_node
+      @parent = parent_node unless @parent
+      @parent.children << self unless @parent.children.include?(self)
+    else
+      @parent = nil
+    end
+  end
+  
+  def parent
+    @parent
+  end
+  
+  def children
+    @children
+  end
+  
+  def value
+    @value
+  end
+end
+
